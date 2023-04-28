bb0ce82a191a670927aeaea96f63701fb00e58c9
mekdes-112 <mekdeseshetu31@gmail.com>
2023-04-29 01:14:33 +0300

    second commit

--- social/twitter/views.py
+++ social/twitter/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Profile, Meep
-from .forms import MeepForm
+from .forms import MeepForm, SignUpForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+
 
 def home(request):
     if request.user.is_authenticated:
@@ -52,3 +56,49 @@ def profile(request, pk):
     else:
         messages.success(request,("You must be logged in"))
         return redirect('home')
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ("You have been logged in. You can start tweeting"))
+            return redirect('home')
+        else:
+            messages.success(request, ("there was an error logging in.Please try again"))
+            return redirect('login')
+    else:
+        return render(request, "login.html", {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You have been logged out."))
+    return redirect('home')
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            #first_name = form.cleaned_data['first_name']
+            #last_name = form.cleaned_data['last_name']
+            #email = form.cleaned_data['email']
+
+
+            user = authenticate(username=username, password=password)
+            login(request,user)
+            messages.success(request, ("You have successfully registered. Welcome"))
+            return redirect('home')
+
+    return render(request, "register.html", {'form':form})
+
+def update_user(request):
+     return render(request, "update_user.html", {})
+
+            
